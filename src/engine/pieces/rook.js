4dddce39de72bf6ec33733763ea2fc69d8cdea5c
Alice Wenban <Alice.Wenban@softwire.com>
2018-07-03 09:11:49 +0100

    rook cant take kinf

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import GameSettings from '../gameSettings';
 import Square from '../square';
+import King from'./king';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -9,6 +10,12 @@ export default class Rook extends Piece {
 
     getAvailableMoves(board) {
         let square = board.findPiece(this);
-        return this.getLateralMoves(board,square);
+        var moves= this.getLateralMoves(board,square);
+        moves.forEach(move=>{
+            if(board.getPiece(move) instanceof King){
+                moves.splice(moves.indexOf(move),1);
+            };
+        });
+        return moves;
     }
 }
